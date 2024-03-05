fdfd80b3db55fa88584f173caf5f128603f8a00e
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-03-05 10:03:32 +0530

    Robo Push

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import posthog from "posthog-js";
+
+posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
+  api_host: "https://app.posthog.com",
+});
+
+posthog.capture("my event", { property: "value" });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
